2d1956686ad6e861359a16b9a799b5bab31aef04
Salman Ahmad Nurhoiriza <salman.nurhoiriza@gmail.com>
2021-10-25 16:48:46 +0700

    Add shared module to dependencies

--- features/home/lib/home.dart
+++ features/home/lib/home.dart
@@ -1,9 +1,16 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:home/presentation/ui/home_page.dart';
+import 'package:shared/common/common.dart';
 
 class FeatureHomeModule extends Module {
   @override
   List<Bind> get binds => [];
 
   @override
-  List<ModularRoute> get routes => [];
+  List<ModularRoute> get routes => [
+        ChildRoute(
+          Modular.get<NamedRoutes>().homePage,
+          child: (context, arg) => const HomePage(),
+        ),
+      ];
 }
