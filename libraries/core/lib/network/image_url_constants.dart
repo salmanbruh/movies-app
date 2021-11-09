ad8318ce4298a6e05e3ca6d564a40164a4ecc1ce
Salman Ahmad Nurhoiriza <salman.nurhoiriza@gmail.com>
2021-11-09 09:40:19 +0700

    Add image urls

---
+++ libraries/core/lib/network/image_url_constants.dart
@@ -0,0 +1,9 @@
+class ImageUrlConstants {
+  static String basePosterUrlDev = "https://image.tmdb.org/t/p";
+  static String basePosterUrlProd = "https://image.tmdb.org/t/p";
+}
+
+class ImageSizeUrlConstants {
+  static String baseHomePosterSizeUrl = "/w154";
+  static String baseDetailsPosterSizeUrl = "/w500";
+}
