f7b04cf1435d6d5c5bbfe21ddb0abad183b92413
Salman Ahmad Nurhoiriza <salman.nurhoiriza@gmail.com>
2021-12-22 22:12:01 +0700

    Add top rated movies to home page

--- features/movies_list/lib/presentation/ui/home_page_lists/home_top_rated_movies_list.dart
+++ features/movies_list/lib/presentation/ui/home_page_lists/home_top_rated_movies_list.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:movies_list/domain/entities/movies_entity.dart';
+import 'package:movies_list/presentation/bloc/blocs.dart';
+import 'package:movies_list/presentation/ui/movies_lists.dart';
+import 'package:shared/widget/widget.dart';
 
 class HomeTopRatedMoviesList extends StatefulWidget {
   const HomeTopRatedMoviesList({Key? key}) : super(key: key);
@@ -10,6 +16,42 @@ class HomeTopRatedMoviesList extends StatefulWidget {
 class _HomeTopRatedMoviesListState extends State<HomeTopRatedMoviesList> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocProvider(
+      create: (context) =>
+          Modular.get<TopRatedMoviesBloc>()..add(TopRatedMoviesLoadData()),
+      child: HomeMoviesCategoriesList(
+        categoryName: "Top Rated",
+        viewAllOnTapFunction: () {},
+        list: _topRatedMoviesList(context),
+      ),
+    );
+  }
+
+  Widget _topRatedMoviesList(BuildContext context) {
+    return BlocBuilder<TopRatedMoviesBloc, TopRatedMoviesState>(
+      builder: (context, state) {
+        if (state is TopRatedMoviesHasData) {
+          List moviesList = state.props[0] as List;
+          return ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: moviesList.length,
+            itemBuilder: (context, index) {
+              MovieEntity movie = moviesList[index];
+              return HomePageMovieCard(movie: movie);
+            },
+          );
+        } else if (state is TopRatedMoviesLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is TopRatedMoviesError) {
+          return Center(child: Text(state.message));
+        } else if (state is TopRatedMoviesNoData) {
+          return Center(child: Text(state.message));
+        } else if (state is TopRatedMoviesNoInternetConnection) {
+          return Center(child: Text(state.message));
+        } else {
+          return const Text("Unknown Error");
+        }
+      },
+    );
   }
 }
