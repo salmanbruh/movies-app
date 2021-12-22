f3f716d17c3cfca3bed230cd073b41eeb2f1a2b0
Salman Ahmad Nurhoiriza <salman.nurhoiriza@gmail.com>
2021-12-22 21:06:06 +0700

    Add Upcoming movies to home page

--- features/movies_list/lib/presentation/ui/home_page_lists/home_upcoming_movies_list.dart
+++ features/movies_list/lib/presentation/ui/home_page_lists/home_upcoming_movies_list.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:movies_list/movies_list.dart';
+import 'package:movies_list/presentation/bloc/blocs.dart';
+import 'package:shared/widget/widget.dart';
 
 class HomeUpcomingMoviesList extends StatefulWidget {
   const HomeUpcomingMoviesList({Key? key}) : super(key: key);
@@ -11,12 +15,42 @@ class HomeUpcomingMoviesList extends StatefulWidget {
 class _HomeUpcomingMoviesListState extends State<HomeUpcomingMoviesList> {
   @override
   Widget build(BuildContext context) {
-    return HomeMoviesCategoriesList(
-      categoryName: "Upcoming",
-      viewAllOnTapFunction: () {},
-      list: _upcomingMoviesList(context),
+    return BlocProvider(
+      create: (context) =>
+          Modular.get<UpcomingMoviesBloc>()..add(UpcomingMoviesLoadData()),
+      child: HomeMoviesCategoriesList(
+        categoryName: "Upcoming",
+        viewAllOnTapFunction: () {},
+        list: _upcomingMoviesList(context),
+      ),
     );
   }
 
-  Widget _upcomingMoviesList(BuildContext context) => Container();
+  Widget _upcomingMoviesList(BuildContext context) =>
+      BlocBuilder<UpcomingMoviesBloc, UpcomingMoviesState>(
+        builder: (context, state) {
+          if (state is UpcomingMoviesHasData) {
+            List moviesList = state.props[0] as List;
+            return ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: moviesList.length,
+              itemBuilder: (context, index) {
+                MovieEntity movie = moviesList[index];
+                return HomePageMovieCard(movie: movie);
+              },
+            );
+          } else if (state is UpcomingMoviesLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is UpcomingMoviesError) {
+            return Center(child: Text(state.message));
+          } else if (state is UpcomingMoviesNoData) {
+            return Center(child: Text(state.message));
+          } else if (state is UpcomingMoviesNoInternetConnection) {
+            return Center(child: Text(state.message));
+          } else {
+            return const Text("Unknown Error");
+          }
+          ;
+        },
+      );
 }
