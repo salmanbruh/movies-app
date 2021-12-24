39e83fe25ca9f367a8de4276221d46907840371d
Salman Ahmad Nurhoiriza <salman.nurhoiriza@gmail.com>
2021-12-24 09:06:46 +0700

    Remove movie details bloc

--- features/movie_details/lib/presentation/bloc/movie_details_event.dart
+++
@@ -1,4 +0,0 @@
-part of 'movie_details_bloc.dart';
-
-@immutable
-abstract class MovieDetailsEvent {}
--- features/movie_details/lib/presentation/bloc/movie_details_state.dart
+++
@@ -1,6 +0,0 @@
-part of 'movie_details_bloc.dart';
-
-@immutable
-abstract class MovieDetailsState {}
-
-class MovieDetailsInitial extends MovieDetailsState {}
